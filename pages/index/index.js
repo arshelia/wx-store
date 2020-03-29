3d96ffa1e7e5314a0385391861e986cb910f93d9
shelia <shelia.wu4575@gmail.com>
2020-03-30 00:20:35 +0800

    fix(*): update

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,79 @@
+/*
+ * @Author: your name
+ * @Date: 2020-03-26 00:10:23
+ * @LastEditTime: 2020-03-29 00:29:36
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /wx-store/pages/index/index.js
+ */
 //index.js
 //获取应用实例
-const app = getApp()
-
-Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
+const app = getApp();
+import { createPage } from "../../lib/wx-store/index";
+import userStore from "../../store/user";
+Page(
+  createPage(
+    {
+      data: {
+        motto: "Hello World",
+        userInfo: {},
+        hasUserInfo: false,
+        canIUse: wx.canIUse("button.open-type.getUserInfo"),
+      },
+      //事件处理函数
+      bindViewTap: function () {
+        wx.navigateTo({
+          url: "../logs/logs",
+        });
+      },
+      onLoad: function () {
+        if (app.globalData.userInfo) {
           this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
+            userInfo: app.globalData.userInfo,
+            hasUserInfo: true,
+          });
+        } else if (this.data.canIUse) {
+          // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+          // 所以此处加入 callback 以防止这种情况
+          app.userInfoReadyCallback = (res) => {
+            this.setData({
+              userInfo: res.userInfo,
+              hasUserInfo: true,
+            });
+          };
+        } else {
+          // 在没有 open-type=getUserInfo 版本的兼容处理
+          wx.getUserInfo({
+            success: (res) => {
+              app.globalData.userInfo = res.userInfo;
+              this.setData({
+                userInfo: res.userInfo,
+                hasUserInfo: true,
+              });
+            },
+          });
         }
-      })
+        userStore.dispatch("getUserInfo");
+      },
+      getUserInfo: function (e) {
+        console.log(e);
+        app.globalData.userInfo = e.detail.userInfo;
+        this.setData({
+          userInfo: e.detail.userInfo,
+          hasUserInfo: true,
+        });
+      },
+      handleDelete(event) {
+        console.log(event);
+        const index = event.currentTarget.dataset.index;
+        console.log(`====删除${index}===`);
+      },
+      handleAdd() {
+        console.log("add操作");
+      },
+    },
+    {
+      userStore,
     }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
-})
+  )
+);
---
+++ store/user.js
@@ -0,0 +1,44 @@
+/*
+ * @Author: your name
+ * @Date: 2020-03-28 18:32:48
+ * @LastEditTime: 2020-03-29 00:38:08
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /wx-store/store/user.js
+ */
+import { Store } from "../lib/wx-store/index";
+import { type } from "../lib/wx-store/utils";
+
+const userStore = new Store({
+  state: {
+    userinfo: {},
+    hobbies: ["food", "sports"],
+  },
+  mutations: {
+    AddHobby(state, hobby) {
+      if (type(hobby) !== "array") {
+        state.hobbies.push(hobby);
+      } else {
+        state.hobbies = this.state.hobbies.cacat(hobby);
+      }
+    },
+    setUserinfo(state, userinfo) {
+      state.userinfo = userinfo;
+    },
+  },
+  actions: {
+    getUserInfo() {
+      const that = arguments[0];
+      setTimeout(() => {
+        console.log("===fake异步获取数据===");
+        const data = {
+          nickname: "shelia",
+          city: "shenzhen",
+        };
+        that.commit("setUserinfo", data);
+      }, 2000);
+    },
+  },
+});
+
+export default userStore;
